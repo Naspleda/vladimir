57f3f5ff40feca1fa608ffca4295837cca2f270c
Naspleda <naspledachris@gmail.com>
2025-06-06 20:44:24 -0300

    fix: clean code and add words in hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+export function Hero() {
+
+return (
+  <>
+  <div className="text-center pt-20 text-3xl md:text-6xl lg:text-6xl font-bold tracking-tight">
+    <span className="bg-gradient-to-r from-[#f1c272] to-[#292512] bg-clip-text text-transparent">
+      KREMLIN TRADING
+      <br/>
+      QUANTUM TECHNOLOGIES
+    </span>
+  </div>
+  </>
+  );
+}
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,3 +1,5 @@
+import { ButtonBorder } from "../buttonBorder";
+import { Hero } from "../Hero";
 import { Navbar } from "../Navbar";
 import Home from "../views/Home";
 
@@ -8,7 +10,8 @@ function Layout() {
     <main className="h-screen flex flex-col bg-black overflow-hidden">
       {/* <div className="pt-[40px] h-[95%] w-[95%] mt-[20px] mx-auto rounded-xl border-2 border-transparent shadow-[0px_0px_19px_6px_rgba(0,_0,_0,_0.1)] bg-radial-center"> */}
         <div className="bg-radial-center">
-          <Navbar />
+          {/* <Navbar /> */}
+          <Hero />
           <Home />
         </div>
       {/* </div> */}

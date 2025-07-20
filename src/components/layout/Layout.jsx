c79c730b31d2564f68d261b6822e703acfc0fd27
Naspleda <naspledachris@gmail.com>
2025-07-20 20:31:21 -0300

    fix: add border

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -9,6 +9,9 @@ function Layout() {
   return (
     <main className="h-screen flex flex-col overflow-hidden">
          {/* bg-radial-center */}
+          <div className="w-[95%] h-[95%] left-1/2 transform -translate-x-1/2 my-6 border border-white/40 rounded-3xl p-3 absolute z-[100] bg-transparent">
+          <h1 className="text-white justify-self-center">MI BOMBI</h1>
+          </div>
         <div className="">
           {/* <Navbar /> */}
           {/* <Hero /> */}
--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -12,8 +12,8 @@ function Home() {
         ">
             {/* <MouseMoveEffect /> */}
             <div className="absolute inset-0 z-10 flex items-center justify-center">
-                <div className="h-[100%] w-full mx-auto  flex flex-col lg:flex-row md:flex-row justify-between gap-4">
-                    <Scene />
+                <div className="h-[100%] w-full mx-auto  flex flex-col lg:flex-row md:flex-row justify-between gap-4 bg-gradient-to-b from-[#00011e] to-[#000000]"> 
+                        <Scene />
                     {/* <div className="h-[80%] w-full flex items-end justify-center text-center px-4 pb-12 z-[60]">
                         <ButtonBorder title="Enter" />
                     </div> */}

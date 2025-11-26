90bdabfd20f84beabd3f4127de44a74a5237a70c
Naspleda <naspledachris@gmail.com>
2025-11-25 21:25:13 -0300

    cambiado la luz a 2. scene con true para probar movimiento

--- public/models/Scene.jsx
+++ public/models/Scene.jsx
@@ -174,7 +174,7 @@ function Scene() {
           target={[0, -0.25, 0]}
           maxPolarAngle={Math.PI - 0.1}
           enableDamping
-          enabled={false}
+          enabled={true}
         />
 
         <PerspectiveCamera ref={camRef} makeDefault fov={50} position={[0, 2, 5]} />

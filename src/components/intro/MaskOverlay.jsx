538a173965130d0413b866d7e5467fe5142e15af
Naspleda <naspledachris@gmail.com>
2025-12-18 23:26:06 -0300

    remove icon music

--- src/components/intro/MaskOverlay.jsx
+++ src/components/intro/MaskOverlay.jsx
@@ -139,11 +139,11 @@ const MaskOverlay = ({ onRevealStart, onRevealComplete }) => {
                             CLICK TO START
                         </span>
                         {/* Sound Icon placeholder */}
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="opacity-80">
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="opacity-80">
                             <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
                             <path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path>
                             <path d="M19.07 4.93a10 10 0 0 1 0 14.14"></path>
-                        </svg>
+                        </svg> */}
                     </div>
                 </Html>
             )}
